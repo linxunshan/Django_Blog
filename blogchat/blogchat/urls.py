5ca6c91a4329b8675b14046cfa1cf9db6144f34b
linxunshan <57648704+linxunshan@users.noreply.github.com>
2022-03-22 23:29:09 -0400

    add register feature and database with basic frontend

--- blogchat/blogchat/urls.py
+++ blogchat/blogchat/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from pyrsistent import inc
+from users import views as users_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', include('blog.urls')),
+    path('register/', users_views.register, name='register'),
+    path('', include('blog.urls')),
 ]   
