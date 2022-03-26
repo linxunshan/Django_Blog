1e41afcc38c9458719c066de46da9ec7b23c162e
linxunshan <57648704+linxunshan@users.noreply.github.com>
2022-03-25 23:37:12 -0400

    add new update delete features for post

--- blogchat/users/models.py
+++ blogchat/users/models.py
@@ -1,6 +1,7 @@
 from tkinter import CASCADE
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 # Create your models here.
 class Profile(models.Model):
@@ -9,3 +10,13 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
+    def save(self, **kwargs):
+        super().save()
+
+        img= Image.open(self.image.path)
+
+        if img.height >300 or img.width >300:
+            update_size =(300,300)
+            img.thumbnail(update_size)
+            img.save(self.image.path)
